2a044e34fcbedad8d47e966cdff34116874a6a90
Julia Kocur <juliakocur@mail.com>
2023-08-10 18:42:05 +0200

    feat: add event listener for dropdown

--- src/components/Header/HeaderNav/HeaderNav.tsx
+++ src/components/Header/HeaderNav/HeaderNav.tsx
@@ -1,14 +1,16 @@
 import './HeaderNav.scss';
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 const HeaderNav = () => {
+const [open, setOpen] = useState(false);
     return (
         <div className="headerNav">
-            <div className="dropdown">
+            <div className={`dropdown ${open? 'active' : ''}`} onClick={()=>{setOpen(!open)}}>
                 <div className="dropbtn">Collection<span className="dropArrow"></span></div> 
                 <div className="dropdownContent">
-                        <a href="#" className="dropLink">Men</a>
-                        <a href="#" className="dropLink">Women</a>
+                        <a href="#" className={`dropLink ${open? 'open' : ''}`}>Men</a>
+                        <a href="#" className={`dropLink ${open? 'open' : ''}`}>Women</a>
                 </div>
             </div>
             <Link to="/about-us" className="link">About us</Link>
