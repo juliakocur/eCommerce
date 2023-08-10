df1451f2850e21b90ad6e442f16ee62d56b92ada
Julia Kocur <juliakocur@mail.com>
2023-08-10 22:45:12 +0200

    feat: create login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,123 @@
 import './LoginPage.scss';
+import { useEffect, useState } from 'react';
+import eyeoff from '../../shared/assets/icons/eye-off.svg';
+import eye from '../../shared/assets/icons/eye.svg';
 
 const LoginPage = () => {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [emailDirty, setEmailDirty] = useState<boolean>(false);
+  const [passwordDirty, setPasswordDirty] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<string>('Все плохо');
+  const [passwordError, setPasswordError] = useState<string>('Все плохо');
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    if (emailError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [emailError, passwordError]);
+
+  const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    const re =
+      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError('Все плохо');
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+
+    const passwordRegex =
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])\S{8,}$/;
+
+    if (!newPassword) {
+      setPasswordError('Пароль не должен быть пустым');
+    } else if (!passwordRegex.test(newPassword)) {
+      setPasswordError('Пароль не соответствует требованиям');
+    } else {
+      setPasswordError('');
+    }
+  };
+
+  const blurHandler = (e: React.FocusEvent<HTMLInputElement>) => {
+    switch (e.target.name) {
+      case 'email':
+        setEmailDirty(true);
+        break;
+      case 'password':
+        setPasswordDirty(true);
+        break;
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div className="wrapperPage">
-      <h1>Login page</h1>
+    <div className="mainPage">
+      <form>
+        <div className="wrapperEmail">
+          <p>
+            Почта <span>*</span>
+          </p>
+
+          <input
+            onChange={(e) => emailHandler(e)}
+            value={email}
+            onBlur={(e) => blurHandler(e)}
+            name="email"
+            type="text"
+            placeholder="Enter your email...."
+          />
+          {emailDirty && emailError && (
+            <div className="error" style={{ color: 'red' }}>
+              {emailError}
+            </div>
+          )}
+        </div>
+        <div className="wrapperPassword">
+          <p>
+            Пароль <span>*</span>
+          </p>
+          <div className="inputPasswordWrapper">
+            <input
+              onChange={(e) => passwordHandler(e)}
+              value={password}
+              onBlur={(e) => blurHandler(e)}
+              name="password"
+              type={showPassword ? 'text' : 'password'}
+              placeholder="Enter your password...."
+            />
+
+            <img
+              src={showPassword ? eye : eyeoff}
+              alt=""
+              style={{ paddingRight: '20px' }}
+              onClick={togglePasswordVisibility}
+            />
+          </div>
+          {passwordDirty && passwordError && (
+            <div className="error" style={{ color: 'red' }}>
+              {passwordError}
+            </div>
+          )}
+        </div>
+
+        <button disabled={!formValid} type="submit">
+          Войти
+        </button>
+      </form>
     </div>
   );
 };
