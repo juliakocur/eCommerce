ddbb79226a1f202d8cbcd3b8ff791d06d3cbd715
Julia Kocur <juliakocur@mail.com>
2023-08-11 15:46:17 +0200

    feat: add link for icon

--- src/components/Header/HeaderIcons/HeaderIcons.tsx
+++ src/components/Header/HeaderIcons/HeaderIcons.tsx
@@ -1,16 +1,19 @@
 import './HeaderIcons.scss';
 import basket from '../../../shared/assets/icons/basket.svg';
 import user from '../../../shared/assets/icons/user.svg';
+import { Link } from 'react-router-dom';
 
 const HeaderIcons = () => {
-    return <>
-        <div className="userAccount">
-            <img src={user} alt="search" className="userImg" />
+    return (
+        <div className="wrapperIcons">
+            <Link to="/user" className="userAccount">
+                <img src={user} alt="search" className="userImg" />Account
+            </Link>
+            <div className="userBasket">
+                <img src={basket} alt="search" className="basketImg" />Basket
+            </div>
         </div>
-        <div className="userBasket">
-            <img src={basket} alt="search" className="basketImg" />
-        </div>
-    </>
+    )
 }
 
 export default HeaderIcons;
