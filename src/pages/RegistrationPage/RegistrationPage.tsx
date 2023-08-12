32d7069da28e54a5a065e299d631d12e09c96334
Julia Kocur <juliakocur@mail.com>
2023-08-12 12:28:04 +0200

    feat: add name, password and e-mail fields

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,11 +1,166 @@
 import './RegistrationPage.scss';
+import { useEffect, useState } from 'react';
+import eyeoff from '../../shared/assets/icons/eye-off.svg';
+import eye from '../../shared/assets/icons/eye.svg';
 
 const RegistrationPage = () => {
-  return (
-    <div className="registrationPage">
-      <h1>Registration page</h1>
-    </div>
-  );
-};
+    const [email, setEmail] = useState<string>();
+    const [password, setPassword] = useState<string>();
+    const [name, setName] = useState<string>();
+    const [emailDirty, setEmailDirty] = useState<boolean>(false);
+    const [passwordDirty, setPasswordDirty] = useState<boolean>(false);
+    const [nameDirty, setNameDirty] = useState<boolean>(false);
+    const [emailError, setEmailError] = useState<string>('Все плохо');
+    const [passwordError, setPasswordError] = useState<string>('Все плохо');
+    const [nameError, setNameError] = useState<string>('Все плохо');
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [formValid, setFormValid] = useState(false);
+  
+    useEffect(() => {
+      if (emailError || passwordError || nameError) {
+        setFormValid(false);
+      } else {
+        setFormValid(true);
+      }
+    }, [emailError, passwordError, nameError]);
+  
+    const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setEmail(e.target.value);
+      const re =
+        /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+  
+      if (!re.test(String(e.target.value).toLowerCase())) {
+        setEmailError('Все плохо');
+      } else {
+        setEmailError('');
+      }
+    };
+  
+    const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newPassword = e.target.value;
+      setPassword(newPassword);
+  
+      const passwordRegex =
+        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])\S{8,}$/;
+  
+      if (!newPassword) {
+        setPasswordError('Пароль не должен быть пустым');
+      } else if (!passwordRegex.test(newPassword)) {
+        setPasswordError('Пароль не соответствует требованиям');
+      } else {
+        setPasswordError('');
+      }
+    };
 
+    const nameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newName = e.target.value;
+      setName(newName);
+
+      const nameRegex = /^[a-zA-Z ]+$/;
+      
+      if (!newName) {
+        setNameError('Имя не должно быть пустое');
+      } else if (!nameRegex.test(newName)) {
+        setNameError('Имя не соответствует требованиям');
+      } else {
+        setNameError('');
+      }
+    }
+  
+    const blurHandler = (e: React.FocusEvent<HTMLInputElement>) => {
+      switch (e.target.name) {
+        case 'email':
+          setEmailDirty(true);
+          break;
+        case 'password':
+          setPasswordDirty(true);
+          break;
+        case 'name':
+          setNameDirty(true);
+          break;
+      }
+    };
+  
+    const togglePasswordVisibility = () => {
+      setShowPassword(!showPassword);
+    };
+  
+    return (
+      <div className="mainPage">
+        <form>
+          <div className="wrapperEmail">
+            <p>
+              Почта <span>*</span>
+            </p>
+  
+            <input
+              onChange={(e) => emailHandler(e)}
+              value={email || ""}
+              onBlur={(e) => blurHandler(e)}
+              name="email"
+              type="text"
+              placeholder="Enter your email...."
+            />
+            {emailDirty && emailError && (
+              <div className="error" style={{ color: 'red' }}>
+                {emailError}
+              </div>
+            )}
+          </div>
+          <div className="wrapperPassword">
+            <p>
+              Пароль <span>*</span>
+            </p>
+            <div className="inputPasswordWrapper">
+              <input
+                onChange={(e) => passwordHandler(e)}
+                value={password || ""}
+                onBlur={(e) => blurHandler(e)}
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Enter your password...."
+              />
+  
+              <img
+                src={showPassword ? eye : eyeoff}
+                alt=""
+                style={{ paddingRight: '20px' }}
+                onClick={togglePasswordVisibility}
+              />
+            </div>
+            {passwordDirty && passwordError && (
+              <div className="error" style={{ color: 'red' }}>
+                {passwordError}
+              </div>
+            )}
+          </div>
+
+          <div className="wrapperName">
+            <p>
+              Name <span>*</span>
+            </p>
+
+            <input
+              onChange={(e) => nameHandler(e)}
+              value={name || ""}
+              onBlur={(e) => blurHandler(e)}
+              name="name"
+              type="text"
+              placeholder="Enter your name...."
+            />
+            {nameDirty && nameError && (
+              <div className="error" style={{ color: 'red' }}>
+                {nameError}
+              </div>
+            )}
+          </div>
+  
+          <button disabled={!formValid} type="submit">
+            Register
+          </button>
+        </form>
+      </div>
+    );
+  };
+  
 export default RegistrationPage;
