cb5b4d41e82e4bcb6b2d5b802374e2bddbd85304
Anton Belski <anton.belski@gmail.com>
2023-08-13 15:32:55 +0300

    feat: add footer component

--- src/components/Footer/Footre.tsx
+++ src/components/Footer/Footre.tsx
@@ -1,9 +1,33 @@
 import './Footer.scss';
+import { Link } from 'react-router-dom';
+import logo from '../../shared/assets/icons/logoFooter.svg';
+import { routes } from '../../routes/AppRouter';
 
 const Footer = () => {
   return (
     <div className="wrapperFooter">
-      <h1>люблю жену</h1>
+      <Link to={`../../${routes.main.path}`}>
+        <img src={logo} alt="" />
+      </Link>
+      <div className="wrapperAddress">
+        <p>
+          1612 Tod Lakes Suite 781 New Heidiport, <div>NE 24589</div>
+        </p>
+      </div>
+      <div className="footerContacts">
+        <div className="tel">
+          <span>Phone</span>
+          <Link to="tel:79001111111" className="linkStyle">
+            +1 (23) 456 78 90
+          </Link>
+        </div>
+        <div className="mail">
+          <span>Email</span>
+          <Link to="mailto:nfo@example.com" className="linkStyle">
+            magandzinshop@myshop.by
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
