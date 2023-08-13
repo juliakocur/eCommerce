a8b54d8e214d8a9ad16fbdba0c39dbc115c081ce
DovguLucky <dovgluch98@gmail.com>
2023-08-13 17:56:40 +0300

    feat: add notfound page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,26 @@
 import './NotFoundPage.scss';
+import { Link } from 'react-router-dom';
+import img from '../../shared/assets/image/bgNotFound.png';
 
 const NotFoundPage = () => {
   return (
     <div className="notFoundPage">
-      <h1>Not found page1</h1>
+      <div className="bcgNotFound">
+        <img src={img} alt="aa" />
+        <div className="textNotFound">
+          <div className="gradientText">
+            <p>Ой, ошибка</p>
+            <h1 className="errorNumb">404</h1>
+          </div>
+          <p>Мы не смогли найти эту страницу.</p>
+          <p>
+            Перейдите на
+            <Link to="/main" className="linkNotFound">
+              <span> главную страницу</span>
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
