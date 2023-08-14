07d49e3a848292a2c138abc5fef2aec234d93021
DovguLucky <dovgluch98@gmail.com>
2023-08-14 13:35:32 +0300

    fix: not-found-page files

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,21 +1,19 @@
 import './NotFoundPage.scss';
 import { Link } from 'react-router-dom';
-import img from '../../shared/assets/image/bgNotFound.png';
 
 const NotFoundPage = () => {
   return (
     <div className="notFoundPage">
       <div className="bcgNotFound">
-        <img src={img} alt="aa" />
         <div className="textNotFound">
-          <div className="gradientText">
+          <div className="gradientText gradientColor">
             <p>Ой, ошибка</p>
             <h1 className="errorNumb">404</h1>
           </div>
           <p>Мы не смогли найти эту страницу.</p>
           <p>
             Перейдите на
-            <Link to="/main" className="linkNotFound">
+            <Link to="/main" className="linkNotFound gradientColor">
               <span> главную страницу</span>
             </Link>
           </p>
