699087d288f4992367e1c6065c87af35f5de5ae1
Julia Kocur <juliakocur@mail.com>
2023-08-16 12:00:24 +0200

    feat: add image

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,123 +1,18 @@
 import './LoginPage.scss';
-import { useEffect, useState } from 'react';
-import eyeoff from '../../shared/assets/icons/eye-off.svg';
-import eye from '../../shared/assets/icons/eye.svg';
+import loginImg from '../../shared/assets/icons/login.png';
+import FormLogin from '../../components/App/FormLogin/FormLogin';
+
 
 const LoginPage = () => {
-  const [email, setEmail] = useState<string>();
-  const [password, setPassword] = useState<string>();
-  const [emailDirty, setEmailDirty] = useState<boolean>(false);
-  const [passwordDirty, setPasswordDirty] = useState<boolean>(false);
-  const [emailError, setEmailError] = useState<string>('Field must not be empty');
-  const [passwordError, setPasswordError] = useState<string>('Field must not be empty');
-  const [showPassword, setShowPassword] = useState<boolean>(false);
-  const [formValid, setFormValid] = useState(false);
-
-  useEffect(() => {
-    if (emailError || passwordError) {
-      setFormValid(false);
-    } else {
-      setFormValid(true);
-    }
-  }, [emailError, passwordError]);
-
-  const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-    const re =
-      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
-
-    if (!re.test(String(e.target.value).toLowerCase())) {
-      setEmailError('Enter an existing email(e.g., example@mail.com)');
-    } else {
-      setEmailError('');
-    }
-  };
-
-  const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newPassword = e.target.value;
-    setPassword(newPassword);
-
-    const passwordRegex =
-      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])\S{8,}$/;
-
-    if (!newPassword) {
-      setPasswordError('Field must not be empty');
-    } else if (!passwordRegex.test(newPassword)) {
-      setPasswordError('Password must contain minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter and 1 number');
-    } else {
-      setPasswordError('');
-    }
-  };
-
-  const blurHandler = (e: React.FocusEvent<HTMLInputElement>) => {
-    switch (e.target.name) {
-      case 'email':
-        setEmailDirty(true);
-        break;
-      case 'password':
-        setPasswordDirty(true);
-        break;
-    }
-  };
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
   return (
-    <div className="mainPage">
-      <form>
-        <div className="wrapperEmail">
-          <p>
-            Email <span>*</span>
-          </p>
-
-          <input
-            onChange={(e) => emailHandler(e)}
-            value={email || ""}
-            onBlur={(e) => blurHandler(e)}
-            name="email"
-            type="text"
-            placeholder="Enter your email.."
-          />
-          {emailDirty && emailError && (
-            <div className="error" style={{ color: 'red' }}>
-              {emailError}
-            </div>
-          )}
-        </div>
-        <div className="wrapperPassword">
-          <p>
-            Password <span>*</span>
-          </p>
-          <div className="inputPasswordWrapper">
-            <input
-              onChange={(e) => passwordHandler(e)}
-              value={password || ""}
-              onBlur={(e) => blurHandler(e)}
-              name="password"
-              type={showPassword ? 'text' : 'password'}
-              placeholder="Enter your password.."
-            />
-
-            <img
-              src={showPassword ? eye : eyeoff}
-              alt=""
-              style={{ paddingRight: '20px' }}
-              onClick={togglePasswordVisibility}
-            />
-          </div>
-          {passwordDirty && passwordError && (
-            <div className="error" style={{ color: 'red' }}>
-              {passwordError}
-            </div>
-          )}
-        </div>
-
-        <button disabled={!formValid} type="submit">
-          Login
-        </button>
-      </form>
+    <div className="wrapperLogin">
+      <div className="wrapperFormLogin">
+        <div className="titleLogin">Login</div>
+        <FormLogin />
+      </div>
+      <div className="loginImgContainer">
+        <img className="sneakerLoginImg" src={loginImg} alt="" />
+      </div>
     </div>
   );
 };
