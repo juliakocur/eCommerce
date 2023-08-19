432a3a4dcbe3f439e31deb39fd990b7921718b12
Julia Kocur <juliakocur@mail.com>
2023-08-20 01:07:05 +0200

    feat: add burger-menu

--- src/components/Header/HeaderNav/HeaderNav.tsx
+++ src/components/Header/HeaderNav/HeaderNav.tsx
@@ -5,6 +5,7 @@ import arrow from '../../../shared/assets/icons/arrow.svg';
 
 const HeaderNav = () => {
   const [open, setOpen] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
   const [category, setCategory] = useState('Categories');
 
   const clickHandler = (cat: string) => {
@@ -13,48 +14,56 @@ const HeaderNav = () => {
   };
 
   return (
-    <div className="headerNav">
-      <div
-        className={`dropdown`}
-        onClick={() => {
-          setOpen(!open);
-        }}
-      >
-        <div className="dropBtn">
-          {category}
-          <img
-            src={arrow}
-            alt=""
-            className={`arrow ${open ? 'upArrow' : ''}`}
-          />
-        </div>
-        {!!open && (
-          <div className="dropdownContent">
-            <div className={`dropLink`} onClick={() => clickHandler('Men')}>
-              Men
-            </div>
-            <div className={`dropLink`} onClick={() => clickHandler('Women')}>
-              Women
-            </div>
-            <div
-              className={`dropLink`}
-              onClick={() => clickHandler('Categories')}
-            >
-              Categories
-            </div>
+    <>
+      <div className={`headerNav ${openMenu ? 'menuOpen' : ''}`}>
+        <div
+          className={`dropdown`}
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          <div className="dropBtn">
+            {category}
+            <img
+              src={arrow}
+              alt=""
+              className={`arrow ${open ? 'upArrow' : ''}`}
+            />
           </div>
-        )}
+          {!!open && (
+            <div className="dropdownContent">
+              <div className={`dropLink`} onClick={() => clickHandler('Men')}>
+                Men
+              </div>
+              <div className={`dropLink`} onClick={() => clickHandler('Women')}>
+                Women
+              </div>
+              <div
+                className={`dropLink`}
+                onClick={() => clickHandler('Categories')}
+              >
+                Categories
+              </div>
+            </div>
+          )}
+        </div>
+        <Link to="/about-us" className="link">
+          About us
+        </Link>
+        <Link to="/login" className="link">
+          Login
+        </Link>
+        <Link to="/registration" className="link">
+          Registration
+        </Link>
       </div>
-      <Link to="/about-us" className="link">
-        About us
-      </Link>
-      <Link to="/login" className="link">
-        Login
-      </Link>
-      <Link to="/registration" className="link">
-        Registration
-      </Link>
-    </div>
+      <button
+        className={`burgerBtn ${openMenu ? 'upBurger' : ''}`}
+        onClick={() => {
+          setOpenMenu(!openMenu);
+        }}
+      ></button>
+    </>
   );
 };
 
