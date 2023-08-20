02307f5ffe91cf5a869e1a675553f0c23774134a
Anton Belski <anton.belski@gmail.com>
2023-08-20 17:41:28 +0300

    feat: add notification component

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import './style.scss';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <Provider store={store}>
+    <ToastContainer />
     <App />
   </Provider>
 );
