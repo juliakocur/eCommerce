d756494dd4ccdb440a91646a279863ee75454a3f
Anton Belski <anton.belski@gmail.com>
2023-08-20 18:35:12 +0300

    feat: protected routes, logout

--- src/components/Header/HeaderIcons/HeaderIcons.tsx
+++ src/components/Header/HeaderIcons/HeaderIcons.tsx
@@ -2,14 +2,19 @@ import './HeaderIcons.scss';
 import basket from '../../../shared/assets/icons/basket.svg';
 import user from '../../../shared/assets/icons/user.svg';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../../store';
 
 const HeaderIcons = () => {
+  const { userId } = useAppSelector((state) => state.auth);
+
   return (
     <div className="wrapperIcons">
-      <Link to="/user" className="userAccount">
-        <img src={user} alt="search" className="userImg" />
-        Account
-      </Link>
+      {userId && (
+        <Link to="/user" className="userAccount">
+          <img src={user} alt="search" className="userImg" />
+          Account
+        </Link>
+      )}
       <Link to="/cart" className="userBasket">
         <img src={basket} alt="search" className="basketImg" />
         Basket
