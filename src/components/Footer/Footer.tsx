7f4b9c22fae4235902a85abfa903c1c04222dbfe
Anton Belski <anton.belski@gmail.com>
2023-08-20 14:51:00 +0300

    fix: change footer,header styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,27 +5,29 @@ import { routes } from '../../routes/AppRouter';
 
 const Footer = () => {
   return (
-    <div className="wrapperFooter">
-      <Link to={`../../${routes.main.path}`}>
-        <img src={logo} alt="" />
-      </Link>
-      <div className="wrapperAddress">
-        <div className="adressText">
-          1612 Tod Lakes Suite 781 New Heidiport, <div>NE 24589</div>
+    <div className="wrapperBg">
+      <div className="wrapperFooter">
+        <Link to={`../../${routes.main.path}`}>
+          <img src={logo} alt="" />
+        </Link>
+        <div className="wrapperAddress">
+          <div className="addressText">
+            1612 Tod Lakes Suite 781 New Heidiport, <div>NE 24589</div>
+          </div>
         </div>
-      </div>
-      <div className="footerContacts">
-        <div className="tel">
-          <span>Phone</span>
-          <Link to="tel:79001111111" className="linkStyle">
-            +1 (23) 456 78 90
-          </Link>
-        </div>
-        <div className="mail">
-          <span>Email</span>
-          <Link to="mailto:nfo@example.com" className="linkStyle">
-            magandzinshop@myshop.by
-          </Link>
+        <div className="footerContacts">
+          <div className="tel">
+            <span>Phone</span>
+            <Link to="tel:79001111111" className="linkStyle">
+              +1 (23) 456 78 90
+            </Link>
+          </div>
+          <div className="mail">
+            <span>Email</span>
+            <Link to="mailto:nfo@example.com" className="linkStyle">
+              magandzinshop@myshop.by
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,22 @@
+import { Link } from 'react-router-dom';
+import logoHeader from '../../routes/../shared/assets/icons/logoHeader.svg';
+import { routes } from '../../routes/AppRouter';
 import './Header.scss';
 import HeaderIcons from './HeaderIcons/HeaderIcons';
 import HeaderNav from './HeaderNav/HeaderNav';
 import HeaderSearch from './HeaderSearch/HeaderSearch';
-import logoHeader from '../../routes/../shared/assets/icons/logoHeader.svg';
-import { Link } from 'react-router-dom';
-import { routes } from '../../routes/AppRouter';
 
 const Header = () => {
   return (
-    <div className="wrapperHeader">
-      <Link to={`../../${routes.main.path}`} className="headerLogo">
-        <img src={logoHeader} alt="" />
-      </Link>
-      <HeaderNav />
-      <HeaderSearch />
-      <HeaderIcons />
+    <div className="wrapperBorder">
+      <div className="wrapperHeader">
+        <Link to={`../../${routes.main.path}`} className="headerLogo">
+          <img src={logoHeader} alt="" />
+        </Link>
+        <HeaderNav />
+        <HeaderSearch />
+        <HeaderIcons />
+      </div>
     </div>
   );
 };
